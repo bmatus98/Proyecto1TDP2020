aada020dc3c0c3f1d862f72e51ccd79ba1471fae
bmatus98 <bmatus98@gmail.com>
2020-09-24 08:28:18 -0300

    Primer commit, inciso e

---
+++ Cadena.java
@@ -0,0 +1,16 @@
+
+public class Cadena {
+
+	private String cadena;
+	
+	public Cadena(String c) {
+		cadena=c;
+	}
+	
+	public void setCadena(String c) {
+		cadena=c;
+	}
+	public String getCadena() {
+		return cadena;
+	}
+}
---
+++ Tester.java
@@ -0,0 +1,10 @@
+
+public class Tester {
+
+	public static void main(String[] args) {
+		Cadena obj = new Cadena("original");
+		System.out.println(obj.getCadena());
+
+	}
+
+}
